ba0eafce60b943a8b67f279de2d4a9ca33c0b536
SugmaGrindset <shaequan26@hotmail.co.uk>
2024-04-29 19:41:33 +0100

    getComputerChoice complete

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function getComputerChoice(){
-    
+    let randomNumber = Math.floor(math.random()*3);
+    switch (randomNumber){
+        case 0:
+            return 'Rock'
+        break;
+        case 1:
+            return 'Paper'
+        break;
+        case 2:
+            return 'Scissors'
+        break;
+    }
 }
